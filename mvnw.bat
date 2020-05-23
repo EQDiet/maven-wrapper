c4e176b32943790ad614aa1beb7299955f28d275
EQDiet <EQDiet@protonmail.com>
2020-05-24 00:48:26 +0200

    Update mvnw.bat

--- mvnw.bat
+++ mvnw.bat
@@ -35,7 +35,6 @@
 
 @REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on'
 @echo off
-@REM set title of command window
 @REM enable echoing by setting MAVEN_BATCH_ECHO to 'on'
 @if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
 
